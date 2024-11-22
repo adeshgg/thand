af6417737d956889006358c52a2d2086a532eea4
Adesh <adesh02092000@gmail.com>
2024-11-22 17:36:52 +0530

    Connect Database Drizzle

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,10 @@
 import GitHub from '@auth/core/providers/github'
 import { NextAuthConfig } from 'next-auth'
-// import { DrizzleAdapter } from '@auth/drizzle-adapter'
-// import { db } from './db'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from './db'
 
 export default {
-  // adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db),
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID,
